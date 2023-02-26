d12442e53cb2a2ea67bc77bf6e644d98ce8af489
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-02-26 18:19:59 +0700

    Update PopD.java

--- JAVA_OOP/StackCalc/src/operation/PopD.java
+++ JAVA_OOP/StackCalc/src/operation/PopD.java
@@ -1,13 +1,16 @@
 package operation;
 
 import calcException.ReferenceToEmptyStack;
+import main.Main;
 import runtimeContext.RuntimeContext;
 import stack.MyStack;
+
+import java.util.logging.Level;
 
 public class PopD implements StackOperation<Double>{
     @Override
     public void apply(MyStack<Double> stack, RuntimeContext<Double> context) throws ReferenceToEmptyStack {
         stack.print();
-        stack.pop();
+        Main.logger.log(Level.INFO, "Pop'ed: "+stack.pop());
     }
 }
