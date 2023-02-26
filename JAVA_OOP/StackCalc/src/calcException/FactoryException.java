8b669fc3facbb5df03a4a7eac2b93b8f2e3ed616
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-02-26 18:12:27 +0700

    Update FactoryException.java

--- JAVA_OOP/StackCalc/src/calcException/FactoryException.java
+++ JAVA_OOP/StackCalc/src/calcException/FactoryException.java
@@ -3,6 +3,6 @@ package calcException;
 public class FactoryException extends Throwable{
    @Override
    public String getMessage(){
-       return "FactoryException: ";
+       return "FactoryException.";
    }
 }
