e9f08cfd44a7c1b0f1899e99a3645d86c6ecd16e
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-02-26 18:21:03 +0700

    Update StackOperation.java

--- JAVA_OOP/StackCalc/src/operation/StackOperation.java
+++ JAVA_OOP/StackCalc/src/operation/StackOperation.java
@@ -2,10 +2,10 @@ package operation;
 
 import calcException.OperationException;
 import calcException.ReferenceToEmptyStack;
+import calcException.WrongFormatInputData;
 import runtimeContext.RuntimeContext;
-import runtimeContext.RuntimeContextD;
 import stack.MyStack;
 
 public interface StackOperation<T> extends Operation<T> {
-    void apply(MyStack<T> stack, RuntimeContext<T> context) throws OperationException, ReferenceToEmptyStack;
+    void apply(MyStack<T> stack, RuntimeContext<T> context) throws OperationException, ReferenceToEmptyStack, WrongFormatInputData;
 }
