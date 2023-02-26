538f2c604c116ca7ca569e48184b0d4de4c9d450
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-02-26 18:21:51 +0700

    Update RuntimeContext.java

--- JAVA_OOP/StackCalc/src/runtimeContext/RuntimeContext.java
+++ JAVA_OOP/StackCalc/src/runtimeContext/RuntimeContext.java
@@ -1,6 +1,17 @@
 package runtimeContext;
 
 public interface RuntimeContext<T> {
+    /**
+     * DEFINE A 4
+     * DEFINE A 5
+     * PUSH A
+     * PRINT -> 5.0
+     * ------------
+     * DEFINE 45 35
+     * PUSH 45
+     * PRINT -> 35.0
+     */
     public void define(String key, T value);
+
     public T getIfDefined(String key);
 }
