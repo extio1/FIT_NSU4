d0018c802a56cb499d4c02cf4b3710f1548712fe
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-02-27 21:20:21 +0700

    Create slayrow.c

---
+++ OPP/SLAY/slayrow.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EPSILON 0.00005
+#define TAY 0.01
+
+void prod_mat_line(double* mat, double* line){
+
+}
+
+void prod_scal_line(double scal, double* line){
+
+}
+
+void minus_line_line(double* line1, double* line2){
+	
+}
+
+int main(){
+	return 0;
+}
