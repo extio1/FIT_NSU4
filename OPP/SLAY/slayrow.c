bce853d9564a9654787da586f094ab8c454f9159
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-01 20:16:04 +0700

    Update slayrow.c

--- OPP/SLAY/slayrow.c
+++ OPP/SLAY/slayrow.c
@@ -4,18 +4,32 @@
 #define EPSILON 0.00005
 #define TAY 0.01
 
-void prod_mat_line(double* mat, double* line){
+void prod_mat_line(double* mat, double* line, int dim) {
+	double* staticLine = (double*)malloc(sizeof(double) * dim);
+	for (int i = 0; i < dim; ++i) {
+		staticLine[i] = line[i];
+	}
 
+	for (int i = 0; i < dim; ++i) {
+		for (int j = 0; j < dim; ++j) {
+			line[j] += staticLine[j] * mat[i * dim + j];
+		}
+	}
+	free(staticLine);
 }
 
-void prod_scal_line(double scal, double* line){
-
+void prod_scal_line(double scal, double* line, int dim) {
+	for (int i = 0; i < dim; ++i) {
+		line[i] *= scal;
+	}
 }
 
-void minus_line_line(double* line1, double* line2){
-	
+void minus_line_line(double* line1, double* line2, int dim) {
+	for (int i = 0; i < dim; ++i) {
+		line2[i] -= line1[i];
+	}
 }
 
-int main(){
+int main() {
 	return 0;
 }
