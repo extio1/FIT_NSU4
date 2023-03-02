f31af354120685b10dfa231ea5c0680df67ae20e
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-02 19:45:01 +0700

    Create matrixio.h

---
+++ OPP/SLAY/matrixio.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void entryMatrix(double*, const int, const char*);
+void entryLine(double*, const int, const char*);
+void printMatrix(const double*, const int);
+void printLine(const double*, const int);
