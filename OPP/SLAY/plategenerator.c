d86c74f264d0312b993bc3f708445795359b0627
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-05 21:00:56 +0700

    Create plategenerator.c

---
+++ OPP/SLAY/plategenerator.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+
+int main(){
+	FILE* coefMatrixF;
+	FILE* xLineF;
+
+	if((coefMatrixF = fopen("coefMatrix.txt", "w")) == NULL || (xLineF = fopen("lineAnswer.txt", "w")) == NULL){
+		printf("Error while creating the empty output files.\n");
+		exit(1);
+	}
+
+	int nx, ny;
+	printf("Enter sizes of plate, x and y:\n");
+	scanf("%d %d", &nx, &ny);
+
+	srand(time(NULL));
+	// i - строка
+	// j - столбец
+	for(int i = 0; i < nx*ny; ++i){
+		for(int j = 0; j < nx*ny; ++j){
+			if(i == j){
+				fprintf(coefMatrixF, "%f ", -4.0);
+			}else if ( ((i-j==1) || (j-i==1))){
+				fprintf(coefMatrixF, "%f ", 1.0);
+			}else if((i-j == nx) || (j-i == nx)){
+				fprintf(coefMatrixF, "%f ", 1.0);
+			}
+			else{
+				fprintf(coefMatrixF, "%f ", 0.0);
+			}
+		}
+		fputs("\n", coefMatrixF);
+		fprintf(xLineF, "%f ", (double)( (rand()%10 == 0)*((double)rand()/rand())*(rand()%2*(-1))) );
+	}
+	printf("==========================>DONE!\n");
+
+}
