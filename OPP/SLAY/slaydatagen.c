74d9e5f0d4f69e80a3136a201b370ce137dc2897
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-06 20:32:19 +0700

    Update slaydatagen.c

--- OPP/SLAY/slaydatagen.c
+++ OPP/SLAY/slaydatagen.c
@@ -1,32 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <iostream>
+#include <random>
 
 int main(){
 	FILE* coefMatrixF;
 	FILE* xLineF;
 
-	if((coefMatrixF = fopen("coefMatrix.txt", "w")) == NULL || (xLineF = fopen("xLineF.txt", "w")) == NULL){
+	if((coefMatrixF = fopen("coefMatrix.txt", "w")) == NULL || (xLineF = fopen("lineAnswer.txt", "w")) == NULL){
 		printf("Error while creating the empty output files.\n");
 		exit(1);
 	}
 
 	int dimension;
+	printf("Enter the dimension to generate:\n");
 	scanf("%d", &dimension);
 
 	srand(time(NULL));
+	double* matrix = (double*) malloc(sizeof(double) * (dimension*dimension));
 	for(int i = 0; i < dimension; ++i){
-		for(int j = 0; j < dimension; ++j){
-			if(i != j){
-				fprintf(coefMatrixF, "%f ", (double)rand()/rand());
-			}
-			else{
-				fprintf(coefMatrixF, "%f ", (double)rand()/rand()+dimension);
+		for(int j = i; j < dimension; ++j){
+			double randomValue = (double)(rand())/RAND_MAX*2 - 1;
+			if(i==j){
+				matrix[i * dimension + j] = randomValue + dimension + dimension;
+			} else {
+				matrix[i * dimension + j] = randomValue;
+				matrix[j * dimension + i] = randomValue;
 			}
 		}
-		fputs("\n", coefMatrixF);
-		fprintf(xLineF, "%f ", (double)rand()/rand());
 	}
 
+	for(int i = 0; i < dimension; ++i){
+		for(int j = 0; j < dimension; ++j){
+			fprintf(coefMatrixF, "%f ", matrix[i * dimension + j]);
+		}
+		fputs("\n", coefMatrixF);
+		fprintf(xLineF, "%f ", 1);
+	}
 
+	free(matrix);
+	printf("==========================>DONE!\n");
+	return 0;
 }
