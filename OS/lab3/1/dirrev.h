b8da9a96df838058713d554484a4f5cce6f326df
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-13 17:17:16 +0700

    Create dirrev.h

---
+++ OS/lab3/1/dirrev.h
@@ -0,0 +1,13 @@
+#ifndef MKDIRRES_H
+#define MKDIRRES_H 1
+
+/*  Reverse "path_to_directory" directory and
+	reverse regular files inside.
+
+	Returns  0 if OK.
+	Returns -1 if Error
+*/
+
+int mkdirrev(const char* path_to_directory);
+
+#endif
