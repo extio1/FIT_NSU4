ad21a88539bbac2152f6ae8268a1956262444c6e
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-14 21:09:33 +0700

    Create page.c

---
+++ OS/lab3/3/page.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+
+void show_maps(char** argv){
+	char path_buff[260];
+
+	if(strcmp(argv[1], "self") == 0){
+		memcpy(path_buff, "/proc/self/maps", 18);
+	} else {
+		sprintf(path_buff, "/proc/%d/maps", atoi(argv[1]));
+	}
+
+	FILE* f = fopen(path_buff, "rb");
+
+	char sym;
+	while(!feof(f)){
+		fread(&sym, 1, 1, f);
+		printf("%c", sym);
+	}
+
+	fclose(f);
+}
+
+void read_procmap(const char* path, unsigned int vaddr){
+	FILE* f = fopen(path, "rb");
+
+	unsigned long offset = vaddr / getpagesize() * 8;
+
+   	if( fseek(f, offset, SEEK_SET) == -1 ){
+      perror("Error while fseek() in /proc/PID/pagemap.\n");
+      return;
+   	}	
+
+	uint64_t pageinfo = 0;
+   	fread(&pageinfo, 8, 1, f);
+
+   	printf("\n----------------------------------------------------\nYes - 1, No - 0\n");
+   	printf("The page is present in RAM: %ld\n", pageinfo&64);
+   	printf("The page is in swap space: %ld\n", pageinfo&63);
+   	printf("The page is a file-mapped page or a shared anonymous page.: %ld\n", pageinfo&61);
+   	//printf("------------------------------------------------------");
+   	printf("The page is exclusively mapped: %ld\n", pageinfo&56);
+   	printf("PTE is soft-dirty: %ld\n", pageinfo&55);
+   /*	if(pageinfo&64)
+   		printf("")
+*/
+   //	(X & ((uint64_t)1<<Y)) >> Y
+	fclose(f);
+}
+
+int main(int argc, char** argv){
+	if(argc != 2){
+		printf("Uncorrect num of arguments.\n");
+		return 0;
+	}
+
+	show_maps(argv);
+
+	char buffer[512];
+	printf("Which address you'd like to examine?: 0x");
+	scanf("%s", &buffer);
+	unsigned long virt_addr = strtol(buffer, NULL, 16);
+
+
+	char path_buff[260];
+	if(strcmp(argv[1], "self") == 0){
+		memcpy(path_buff, "/proc/self/maps", 18);
+	} else {
+		sprintf(path_buff, "/proc/%d/maps", atoi(argv[1]));
+	}
+
+	read_procmap(path_buff, virt_addr);
+
+	return 0;
+}
