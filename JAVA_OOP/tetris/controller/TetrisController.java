b4e7d1313c07be679b7417822862963ca6279ed9
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-24 19:15:51 +0700

    Update TetrisController.java

--- JAVA_OOP/tetris/controller/TetrisController.java
+++ JAVA_OOP/tetris/controller/TetrisController.java
@@ -5,7 +5,7 @@ import tetrisModel.Tetris;
 import static controller.CommandTetris.Left;
 import static controller.CommandTetris.Right;
 
-public class TetrisController implements Controller<Tetris>{
+public class TetrisController implements Controller{
     Tetris myModel = null;
 
     public TetrisController(Tetris _Tetris){
@@ -14,10 +14,15 @@ public class TetrisController implements Controller<Tetris>{
 
     @Override
     public void execute(CommandTetris comm) {
-        switch (comm){
+        switch (comm) {
             case Left -> myModel.left();
             case Right -> myModel.right();
+            case Up -> myModel.up();
+            case Down -> myModel.down();
             case Exit -> myModel.exit();
+            case NewGame -> myModel.newGame();
+            case HighScores -> myModel.highScores();
+            case About -> myModel.about();
         }
     }
 
