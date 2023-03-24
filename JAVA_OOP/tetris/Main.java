9db8e472c18e392773890c3d613ab595dcfbb458
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-24 19:15:09 +0700

    Update Main.java

--- JAVA_OOP/tetris/Main.java
+++ JAVA_OOP/tetris/Main.java
@@ -1,16 +1,29 @@
+import view.GUI.GraphicUI;
 import controller.CommandTetris;
 import controller.Controller;
 import controller.TetrisController;
 import tetrisModel.Tetris;
 
-public class Main {
-    public static void main(String[] args){
-        Tetris game = new Tetris();
-        Controller<Tetris> controller = new TetrisController(game);
+import javax.swing.*;
 
-        game.start();
+import static java.lang.Thread.sleep;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Tetris game = new Tetris();
+        Controller controller = new TetrisController(game);
+
+        GraphicUI ui = new GraphicUI(800, 600, controller, game);
+        ui.setVisibleMainFrame(true);
+
+        game.attach(ui);
+
+        game.turnOn();
 
         controller.execute(CommandTetris.Left);
+        sleep(60000);
         controller.execute(CommandTetris.Exit);
+
     }
 }
