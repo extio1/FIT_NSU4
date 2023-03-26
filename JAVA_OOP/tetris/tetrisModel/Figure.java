7b6ffcdfbf6758d4c1826880646a9b0fd0ca2b56
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-26 21:03:48 +0700

    Update Figure.java

--- JAVA_OOP/tetris/tetrisModel/Figure.java
+++ JAVA_OOP/tetris/tetrisModel/Figure.java
@@ -1,17 +1,83 @@
 package tetrisModel;
 
 public enum Figure {
-    leftAngle   (new int[]{0,1,1,1,  0,0,0,1}),
-    rightAngle  (new int[]{0,0,0,1,  0,1,1,1}),
-    leftSnake   (new int[]{0,1,1,0,  0,0,1,1}),
-    rightSnake  (new int[]{0,0,1,1,  1,1,0,0}),
-    pedestal    (new int[]{0,0,1,0,  0,1,1,1}),
-    stick       (new int[]{0,0,0,0,  1,1,1,1});
+    leftAngle   (new FigureParams(5, 0, (byte) 1,
+                 new byte[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+                 new byte[][]{{2, 3}, {3, 2}, {2, 3}, {3, 2}},
+                 new byte[][]{{1, 0, 1, 0, 1, 1},
+                             {1, 1, 1, 1, 0, 0},
+                             {1, 1, 0, 1, 0, 1},
+                             {0, 0, 1, 1, 1, 1}})),
 
-    private final int[] innerView;
-    Figure(int[] view){
-        innerView = view;
+    rightAngle  (new FigureParams(5, 0, (byte) 1,
+                 new byte[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+                 new byte[][]{{2, 3}, {3, 2}, {2, 3}, {3, 2}},
+                 new byte[][]{{1, 0, 1, 0, 1, 1},
+                             {1, 1, 1, 1, 0, 0},
+                             {1, 1, 0, 1, 0, 1},
+                             {0, 0, 1, 1, 1, 1}})),
+
+    leftSnake   (new FigureParams(5, 0, (byte) 1,
+                 new byte[][]{{0, 0}, {0, 0}},
+                 new byte[][]{{2, 3}, {3, 2}},
+                 new byte[][]{{1, 0, 1, 1, 0, 1},
+                             {0, 1, 1, 1, 1, 0}})),
+
+    rightSnake  (new FigureParams(5, 0, (byte) 1,
+                 new byte[][]{{0, 0}, {0, 0}},
+                 new byte[][]{{2, 3}, {3, 2}},
+                 new byte[][]{{0, 1, 1, 1, 1, 0},
+                             {1, 1, 0, 0, 1, 1}})),
+
+    pedestal    (new FigureParams(5, 0, (byte) 1,
+                 new byte[][]{{-1, 0}, {1, 0}, {-1, 0}, {1, 0}},
+                 new byte[][]{{3, 2}, {2, 3}, {2, 3}, {2, 3}},
+                 new byte[][]{{0, 1, 0, 1, 1, 1},
+                              {1, 0, 1, 1, 1, 0},
+                              {1, 1, 1, 0, 1, 0},
+                              {0, 1, 1, 1, 0, 1} })),
+
+    stick       (new FigureParams(5, 0, (byte) 1,
+                 new byte[][]{{0, 2}, {0, -1}, {1, 1}, {-1, -2}},
+                 new byte[][]{{1, 4}, {4, 1}, {1, 4}, {4, 1}},
+                 new byte[][]{{1, 1, 1, 1},
+                              {1, 1, 1, 1},
+                              {1, 1, 1, 1},
+                              {1, 1, 1, 1} })),
+
+    cube        (new FigureParams(5, 0, (byte) 1,
+                 new byte[][]{{0, 0}},
+                 new byte[][]{{2, 2}},
+                 new byte[][]{{1, 1, 1, 1}}));
+
+    public final FigureParams params;
+    Figure(FigureParams params){
+        this.params = params;
     }
 
+    static private int[] calcBuffer = new int[2];
+    public int[] getCoordinatesInFieldAfterRotate(){
+        int[] buff = params.getNextConditionPos();
+        calcBuffer[0] = buff[0] + params.getPosX();
+        calcBuffer[1] = buff[1] + params.getPosY();
+        return calcBuffer;
+    }
+
+    public void refreshFigure(){
+        params.refresh();
+    }
+
+    public boolean isItAreRealPartOfFigure(int x, int y){
+        return (params.getCondition()[y * params.getWidth() + x] == 1);
+    }
+
+    public void rotate(){
+        params.setPosX(params.getPointerShiftToNextPos()[0] + params.getPosX());
+        params.setPosY(params.getPointerShiftToNextPos()[1] + params.getPosY());
+    }
+
+    public void moveDown(){
+        params.setPosY(params.getPosY()+1);
+    }
 
 }
