4ec22d5ea95cdc7e1b12c511aecd24d4f18cd1da
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-26 21:02:56 +0700

    Update Tetris.java

--- JAVA_OOP/tetris/tetrisModel/Tetris.java
+++ JAVA_OOP/tetris/tetrisModel/Tetris.java
@@ -1,35 +1,53 @@
 package tetrisModel;
 
-import controller.CommandTetris;
 import view.observation.Observer;
 import view.observation.Subject;
 
 import java.util.*;
 
 public class Tetris implements Subject {
+    private static final byte N_FIGURES_IN_TETRIS = 7;
+    private static int X_SIZE_FLD;
+    private static int Y_SIZE_FLD;
+
+    private final List<Observer> observers = new ArrayList<>();
+    private final Package packageToObserver;
 
     private volatile boolean gameRunning = false;
 
-    private int figureCounter = 0;
-    private int figureSpeed = 1000; //sleep in ms
-    private final List<Observer> observers = new ArrayList<>();
-    
-    private int[] gameField;
-    // there should be two data: outer and for observers
+    private final Random randomGenerator;
+    private int figureCounter;
+    private int figureFallingPeriod = 1000; //sleep in ms
 
-    Thread figureFalls;
+    private final Figure[] tetrisFigures;
+    private Figure fallingFigure;
+
+    private final int[][] gameField;
+    // there should be two data: inner and for observers
+
+    private Thread figureFalls;
 
     public Tetris(){
-        final int FIELD_SIZE_X = 10;
-        final int FIELD_SIZE_Y = 20;
+        this(10, 20);
+    }
 
-        gameField = new int[FIELD_SIZE_X * FIELD_SIZE_Y];
+    public Tetris(int xDim, int yDim){
+        gameField = new int[xDim][yDim];
 
-        for(int i = 0; i < FIELD_SIZE_X; ++i){
-            for(int j = 0; j < FIELD_SIZE_Y; ++j){
-                gameField[i * FIELD_SIZE_X + j] = 0;
+        X_SIZE_FLD = xDim;
+        Y_SIZE_FLD = yDim;
+
+        for(int i = 0; i < xDim; ++i){
+            for(int j = 0; j < yDim; ++j){
+                gameField[i][j] = 0;
             }
         }
+
+        figureCounter = 1;
+        tetrisFigures = Figure.values();
+        packageToObserver = new Package(X_SIZE_FLD, Y_SIZE_FLD);
+        randomGenerator = new Random();
+        generateFigureFallingThread();
     }
 
     private void generateFigureFallingThread(){
@@ -37,10 +55,10 @@ public class Tetris implements Subject {
             @Override
             public void run() {
                 while(!Thread.interrupted() || gameRunning) {
-                    System.out.println("GO DOWN");
+                    down();
                     signalyzeAll();
                     try {
-                        Thread.sleep(figureSpeed);
+                        Thread.sleep(figureFallingPeriod);
                     } catch (InterruptedException e) {
                         if(gameRunning) {
                             System.out.println(e.getMessage());
@@ -50,13 +68,16 @@ public class Tetris implements Subject {
                 }
             }
         });
+
         figureFalls.setPriority(Thread.MAX_PRIORITY);
     }
 
     public void turnOn(){
         gameRunning = true;
+        generateNewFallingFigure();
         figureFalls.start();
     }
+
     public void pauseGame(){
         gameRunning = false;
     }
@@ -110,6 +131,34 @@ public class Tetris implements Subject {
 
     public void up(){
         if(gameRunning) {
+            // checking if is it possible to rotate the figure
+            int[] newPointerCoordinates = fallingFigure.getCoordinatesInFieldAfterRotate();
+            int xPos = newPointerCoordinates[0];
+            int yPos = newPointerCoordinates[1];
+
+            // traverse cells of the figure after rotate and check if some figures have already located here
+            for(int i = 0; i < fallingFigure.params.getLength(); ++i){
+                for(int j = 0; j < fallingFigure.params.getWidth(); ++j){
+                    if(gameField[yPos+i][xPos + j] != 0 && fallingFigure.isItAreRealPartOfFigure(i, j)){
+                        return;
+                    }
+                }
+            }
+
+            if (xPos < 0){
+                // checking if it's possible to move the figure to some offset right
+                if(gameField[yPos][xPos + fallingFigure.params.getWidth() + Math.abs(xPos)] != 0){
+                    return;
+                }
+            } else if (xPos > X_SIZE_FLD) {
+                // checking if it's possible to move the figure to some offset left
+                if(gameField[yPos][xPos - (xPos - X_SIZE_FLD)] != 0){
+                    return;
+                }
+            }
+
+            fallingFigure.rotate();
+
             System.out.println("DO UP");
             signalyzeAll();
         }
@@ -117,8 +166,14 @@ public class Tetris implements Subject {
 
     public void down(){
         if(gameRunning) {
-            System.out.println("DO DOWN");
+            if(!isFigureFell()) {
+                fallingFigure.moveDown();
+            } else {
+                figureFell();
+                generateNewFallingFigure();
+            }
 
+            System.out.println("DO DOWN");
             signalyzeAll();
         }
     }
@@ -142,6 +197,55 @@ public class Tetris implements Subject {
 
     @Override
     public Object getInfo() {
-        return gameField;
+        packageToObserver.setFigure(fallingFigure);
+        packageToObserver.setGameField(gameField);
+        return packageToObserver;
+    }
+
+    private boolean isFigureFell(){
+        int xPos = fallingFigure.params.getPosX();
+        int yPos = fallingFigure.params.getPosY();
+        byte[] figureView = fallingFigure.params.getCondition();
+        int length = fallingFigure.params.getLength();
+        int width = fallingFigure.params.getWidth();
+
+        //if there's something under the figure or next Y-coordinate more that Y_SIZE_FLD then figure fell
+        if(yPos + length >= Y_SIZE_FLD){
+            return true;
+        }
+        System.out.println(xPos+" - "+yPos);
+        for(int i = 0; i < length; ++i){
+            for(int j = 0; j < width; ++j){
+                if(figureView[i*width+j]==1 && gameField[xPos + j][yPos+i+1] != 0){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void generateNewFallingFigure(){
+        figureCounter += 1;
+        fallingFigure = tetrisFigures[(randomGenerator.nextInt(N_FIGURES_IN_TETRIS))];
+        fallingFigure.refreshFigure();
+        System.out.println(fallingFigure);
+    }
+
+    private void figureFell(){
+        int xPos = fallingFigure.params.getPosX();
+        int yPos = fallingFigure.params.getPosY();
+        int length = fallingFigure.params.getLength();
+        int width = fallingFigure.params.getWidth();
+        byte[] figureView = fallingFigure.params.getCondition();
+
+        for(int i = 0; i < length; ++i){
+            for(int j = 0; j < width; ++j){
+                if(figureView[i*width+j]==1) {
+                    gameField[xPos + j][yPos + i] = figureCounter;
+                    ++figureCounter;
+                }
+            }
+        }
     }
 }
