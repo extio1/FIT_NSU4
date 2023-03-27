d455f3f11139a922a272590bb23bb553adaf2694
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-27 22:34:27 +0700

    Create MainWindow.java

---
+++ JAVA_OOP/tetris/view/GUI/MainWindow.java
@@ -0,0 +1,68 @@
+package view.gui;
+
+import controller.CommandTetris;
+import controller.Controller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainWindow extends JFrame {
+    private final Controller controller;
+    private final JLayeredPane lp = getLayeredPane();
+
+    MainWindow(Controller controller){
+        super("Tetris");
+        this.controller = controller;
+        RightBank rb = new RightBank(lp, controller);
+
+        setBounds(600, 100, 600, 800);
+        setBackground(Color.RED);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setIconImage(new ImageIcon("resources/images/main_frame_icon.jpg").getImage());
+        add(rb);
+
+        addKeyListener(new KeyListener()
+        {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_LEFT) controller.execute(CommandTetris.Left);
+                if (e.getKeyCode()==KeyEvent.VK_RIGHT) controller.execute(CommandTetris.Right);
+                if (e.getKeyCode()==KeyEvent.VK_UP) controller.execute(CommandTetris.Up);
+                if (e.getKeyCode()==KeyEvent.VK_DOWN) controller.execute(CommandTetris.Down);
+                if (e.getKeyCode()==KeyEvent.VK_ESCAPE) controller.execute(CommandTetris.Menu);
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        });
+
+        addWindowListener(new WindowListener() {
+            public void windowActivated(WindowEvent event) {}
+            public void windowClosed(WindowEvent event) {}
+            public void windowDeactivated(WindowEvent event) {}
+            public void windowDeiconified(WindowEvent event) {}
+            public void windowIconified(WindowEvent event) {}
+            public void windowOpened(WindowEvent event) {}
+            public void windowClosing(WindowEvent event) {
+                Object[] options = { "Да", "Нет" };
+                int rc = JOptionPane.showOptionDialog(
+                        event.getWindow(), "Закрыть окно?",
+                        "Подтверждение", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        new ImageIcon("resources/images/question_head.png"),
+                        null, options);
+
+                if (rc == 0) {
+                    event.getWindow().setVisible(false);
+                    System.exit(0);
+                }
+            }
+        });
+
+        setFocusable(true);
+        setVisible(true);
+    }
+}
