ea4efd4f1c9b55bc93f1c55a3314c03925910b43
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-30 22:37:17 +0700

    Update Package.java

--- JAVA_OOP/tetris/tetrisModel/Package.java
+++ JAVA_OOP/tetris/tetrisModel/Package.java
@@ -1,6 +1,8 @@
 package tetrisModel;
 
 public class Package {
+    private boolean fieldChanges;
+
     private final int X_SIZE_FLD;
     private final int Y_SIZE_FLD;
 
@@ -8,11 +10,27 @@ public class Package {
     private byte[] figureView;
     private int figureLength;
     private int figureWidth;
+    private int figureNumber;
+    private int figurePosX;
+    private int figurePosY;
 
     Package(int xsf, int ysf){
         X_SIZE_FLD = xsf;
         Y_SIZE_FLD = ysf;
+        fieldChanges = true;
         gameField = new int[X_SIZE_FLD][Y_SIZE_FLD];
+    }
+
+    public void setFigurePosX(int posX){
+        figurePosX = posX;
+    }
+
+    public void setFigurePosY(int posY){
+        figurePosY = posY;
+    }
+
+    public void setFigureNumber(int fn){
+        figureNumber = fn;
     }
 
     public void setGameField(int[][] gf){
@@ -27,5 +45,45 @@ public class Package {
         figureView = figure.params.getCondition().clone();
         figureLength = figure.params.getLength();
         figureWidth = figure.params.getWidth();
+        figurePosX = figure.params.getPosX();
+        figurePosY = figure.params.getPosY();
+        figureNumber = figure.params.getOrdinal();
     }
+
+    public int getFigureNumber() {
+        return figureNumber;
+    }
+
+    public int getFigurePosX() {
+        return figurePosX;
+    }
+
+    public int getFigurePosY() {
+        return figurePosY;
+    }
+
+    public void setFieldChanges(boolean flag){
+        fieldChanges = flag;
+    }
+
+    public boolean isFieldChanges(){
+        return fieldChanges;
+    }
+
+    public int[][] getGameField(){
+        return gameField;
+    }
+
+    public byte[] getFigureView(){
+        return figureView;
+    }
+
+    public int getFigureLength(){
+        return figureLength;
+    }
+
+    public int getFigureWidth() {
+        return figureWidth;
+    }
+
 }
