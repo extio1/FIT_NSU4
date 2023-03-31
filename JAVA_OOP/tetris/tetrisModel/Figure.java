b3eab3b2eb26e24c8919bd0cc463fae218af7c0d
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-31 17:06:12 +0700

    Update Figure.java

--- JAVA_OOP/tetris/tetrisModel/Figure.java
+++ JAVA_OOP/tetris/tetrisModel/Figure.java
@@ -1,65 +1,66 @@
 package tetrisModel;
 
 public enum Figure {
-    leftAngle   (new FigureParams(5, 0, (byte) 1,
-                 new byte[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
-                 new byte[][]{{2, 3}, {3, 2}, {2, 3}, {3, 2}},
-                 new byte[][]{{1, 0, 1, 0, 1, 1},
-                             {1, 1, 1, 1, 0, 0},
-                             {1, 1, 0, 1, 0, 1},
-                             {0, 0, 1, 1, 1, 1}})),
+    leftAngle   (new FigureParams(5, 0, (byte) 4,
+            new byte[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+            new byte[][]{{2, 3}, {3, 2}, {2, 3}, {3, 2}},
+            new byte[][]{{1, 0, 1, 0, 1, 1},
+                    {1, 1, 1, 1, 0, 0},
+                    {1, 1, 0, 1, 0, 1},
+                    {0, 0, 1, 1, 1, 1}})),
 
-    rightAngle  (new FigureParams(5, 0, (byte) 1,
-                 new byte[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
-                 new byte[][]{{2, 3}, {3, 2}, {2, 3}, {3, 2}},
-                 new byte[][]{{1, 0, 1, 0, 1, 1},
-                             {1, 1, 1, 1, 0, 0},
-                             {1, 1, 0, 1, 0, 1},
-                             {0, 0, 1, 1, 1, 1}})),
+    rightAngle  (new FigureParams(5, 0, (byte) 4,
+            new byte[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+            new byte[][]{{2, 3}, {3, 2}, {2, 3}, {3, 2}},
+            new byte[][]{{1, 0, 1, 0, 1, 1},
+                    {1, 1, 1, 1, 0, 0},
+                    {1, 1, 0, 1, 0, 1},
+                    {0, 0, 1, 1, 1, 1}})),
 
-    leftSnake   (new FigureParams(5, 0, (byte) 1,
-                 new byte[][]{{0, 0}, {0, 0}},
-                 new byte[][]{{2, 3}, {3, 2}},
-                 new byte[][]{{1, 0, 1, 1, 0, 1},
-                             {0, 1, 1, 1, 1, 0}})),
+    leftSnake   (new FigureParams(5, 0, (byte) 2,
+            new byte[][]{{0, 0}, {0, 0}},
+            new byte[][]{{2, 3}, {3, 2}},
+            new byte[][]{{1, 0, 1, 1, 0, 1},
+                    {0, 1, 1, 1, 1, 0}})),
 
-    rightSnake  (new FigureParams(5, 0, (byte) 1,
-                 new byte[][]{{0, 0}, {0, 0}},
-                 new byte[][]{{2, 3}, {3, 2}},
-                 new byte[][]{{0, 1, 1, 1, 1, 0},
-                             {1, 1, 0, 0, 1, 1}})),
+    rightSnake  (new FigureParams(5, 0, (byte) 2,
+            new byte[][]{{0, 0}, {0, 0}},
+            new byte[][]{{2, 3}, {3, 2}},
+            new byte[][]{{0, 1, 1, 1, 1, 0},
+                    {1, 1, 0, 0, 1, 1}})),
 
-    pedestal    (new FigureParams(5, 0, (byte) 1,
-                 new byte[][]{{-1, 0}, {1, 0}, {-1, 0}, {1, 0}},
-                 new byte[][]{{3, 2}, {2, 3}, {2, 3}, {2, 3}},
-                 new byte[][]{{0, 1, 0, 1, 1, 1},
-                              {1, 0, 1, 1, 1, 0},
-                              {1, 1, 1, 0, 1, 0},
-                              {0, 1, 1, 1, 0, 1} })),
+    pedestal    (new FigureParams(5, 0, (byte) 4,
+            new byte[][]{{-1, 0}, {1, 0}, {-1, 0}, {1, 0}},
+            new byte[][]{{3, 2}, {2, 3}, {2, 3}, {2, 3}},
+            new byte[][]{{0, 1, 0, 1, 1, 1},
+                    {1, 0, 1, 1, 1, 0},
+                    {1, 1, 1, 0, 1, 0},
+                    {0, 1, 1, 1, 0, 1} })),
 
-    stick       (new FigureParams(5, 0, (byte) 1,
-                 new byte[][]{{0, 2}, {0, -1}, {1, 1}, {-1, -2}},
-                 new byte[][]{{1, 4}, {4, 1}, {1, 4}, {4, 1}},
-                 new byte[][]{{1, 1, 1, 1},
-                              {1, 1, 1, 1},
-                              {1, 1, 1, 1},
-                              {1, 1, 1, 1} })),
+    stick       (new FigureParams(5, 0, (byte) 4,
+            new byte[][]{{0, 2}, {0, -1}, {1, 1}, {-1, -2}},
+            new byte[][]{{1, 4}, {4, 1}, {1, 4}, {4, 1}},
+            new byte[][]{{1, 1, 1, 1},
+                    {1, 1, 1, 1},
+                    {1, 1, 1, 1},
+                    {1, 1, 1, 1} })),
 
     cube        (new FigureParams(5, 0, (byte) 1,
-                 new byte[][]{{0, 0}},
-                 new byte[][]{{2, 2}},
-                 new byte[][]{{1, 1, 1, 1}}));
+            new byte[][]{{0, 0}},
+            new byte[][]{{2, 2}},
+            new byte[][]{{1, 1, 1, 1}}));
 
     public final FigureParams params;
     Figure(FigureParams params){
         this.params = params;
     }
 
-    static private int[] calcBuffer = new int[2];
+    static private final int[] calcBuffer = new int[2];
     public int[] getCoordinatesInFieldAfterRotate(){
-        int[] buff = params.getNextConditionPos();
+        byte[] buff = params.getPointerShiftToNextPos();
         calcBuffer[0] = buff[0] + params.getPosX();
         calcBuffer[1] = buff[1] + params.getPosY();
+
         return calcBuffer;
     }
 
@@ -74,6 +75,7 @@ public enum Figure {
     public void rotate(){
         params.setPosX(params.getPointerShiftToNextPos()[0] + params.getPosX());
         params.setPosY(params.getPointerShiftToNextPos()[1] + params.getPosY());
+        params.setNextCondition();
     }
 
     public void moveRight(){
