88019d67107fccc4fe77da1bfd7011024e0db92d
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-04-21 13:09:16 +0700

    Create View.java

---
+++ JAVA_OOP/tetris/viewer/View.java
@@ -0,0 +1,10 @@
+package viewer;
+
+import observation.Observer;
+
+/**
+ * Самый первый пакет, который приходит от модели имеет состояние authState
+ */
+
+public abstract class View extends Thread implements Observer {
+}
