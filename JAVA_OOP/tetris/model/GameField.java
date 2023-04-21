66dfd7d6f167e6f9c50c22ced1c81f7e7ba0a7bd
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-04-21 13:14:39 +0700

    Update GameField.java

--- JAVA_OOP/tetris/model/GameField.java
+++ JAVA_OOP/tetris/model/GameField.java
@@ -5,8 +5,15 @@ import model.exception.IndexOutOfField;
 
 public interface GameField {
     Object getRepresentation();
-    boolean isCellFree(int x, int y) throws IndexOutOfField;
+    boolean isCellFree(int ... pos) throws IndexOutOfField, DimensionOutOfField;
     void assignValueToPosition(Object value, int ... pos) throws IndexOutOfField, DimensionOutOfField;
     int[] getSizeField();
-    void removeFullLayers();
+    void renew();
+    /**
+     *
+     * @param from current position of the figure
+     * @param length it's lenght
+     * @return num of layers was deleted
+     */
+    int removeFullLayers(int from, int length);
 }
