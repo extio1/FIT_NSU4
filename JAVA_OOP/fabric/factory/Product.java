3798fda03cee56a7b1bbc52eb9d11cc26c4e3a24
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-04-25 20:47:23 +0700

    Create Product.java

---
+++ JAVA_OOP/fabric/factory/Product.java
@@ -0,0 +1,13 @@
+package factory;
+
+public abstract class Product{
+    private final int id;
+
+    public Product(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
