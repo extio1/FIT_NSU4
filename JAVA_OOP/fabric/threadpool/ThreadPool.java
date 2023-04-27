2daca98f2e8a5584aa1c9a68a247b52e4ef0d452
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-04-27 19:03:49 +0700

    Update ThreadPool.java

--- JAVA_OOP/fabric/threadpool/ThreadPool.java
+++ JAVA_OOP/fabric/threadpool/ThreadPool.java
@@ -1,5 +1,50 @@
 package threadpool;
 
-public class ThreadPool {
+import java.util.ArrayDeque;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.Executor;
+
+public class PoolExecutor implements Executor {
+    private final Queue<Runnable> tasks = new ArrayDeque<>();
+    private final Worker[] workers;
+
+    public PoolExecutor(int nThreads){
+        workers = new Worker[nThreads];
+        for(int i = 0; i < nThreads; ++i)
+            workers[i] = new Worker(workers);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if(command == null){
+            throw new NullPointerException();
+        }
+
+        tasks.add(command);
+    }
+
+    private class Worker extends Thread{
+        private final Object syncObj;
+        public Worker(Object syncObj){
+            this.syncObj = syncObj;
+        }
+
+        @Override
+        public void run(){
+            while(!Thread.interrupted()){
+                try {
+                    syncObj.wait();
+                } catch (InterruptedException e) {
+                    break;
+                }
+
+                synchronized (tasks) {
+                    Objects.requireNonNull(tasks.poll()).run();
+                }
+            }
+            System.out.println("Thread "+Thread.currentThread()+" interrupted");
+        }
+    }
 
 }
