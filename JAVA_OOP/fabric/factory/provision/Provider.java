5bce0358d98eb62c39868b43deb927d42bddff0e
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-04-29 14:35:05 +0700

    Update Provider.java

--- JAVA_OOP/fabric/factory/provision/Provider.java
+++ JAVA_OOP/fabric/factory/provision/Provider.java
@@ -1,35 +1,36 @@
 package factory.provision;
 
+import factory.InteractivePerformer;
 import factory.Product;
 import factory.Storage;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class Provider <T extends Product> {
+public class Provider <T extends Product> implements InteractivePerformer {
     private volatile int period; //period in ms
     private int counterProduct = 0;
-    ManufacturingProcess manufacturingProcess;
     Thread executor;
 
     Storage<T> storage;
 
-    public Provider(int period, Class<? extends Product> providingProduct, Storage<T> storage){
+    public Provider(int period, Class<? extends Product> providingProduct, Storage<T> storage, int providerSerialNumber){
         this.period = period;
         this.storage = storage;
-        executor = new Thread(new ManufacturingProcess(providingProduct.toString()));
+        executor = new Thread(new ManufacturingProcess(providingProduct.getTypeName()));
+        executor.setName("Provider of "+providingProduct+" #"+providerSerialNumber);
     }
 
-    public void startSupplies(){
+    @Override
+    public void startPerform(){
         executor.start();
     }
 
-    public void stopSupplies(){
+    @Override
+    public void stopPerform(){
         executor.interrupt();
     }
 
+    @Override
     public void changePeriod(int newPeriod) {
         period = newPeriod;
     }
@@ -46,7 +47,7 @@ public class Provider <T extends Product> {
             while(!Thread.interrupted()) {
                 try {
                     Thread.sleep(period);
-                    doJob();
+                    doCreation();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -54,14 +55,14 @@ public class Provider <T extends Product> {
         }
 
         @SuppressWarnings("unchecked")
-        private void doJob() {
+        private void doCreation() {
             try {
                 Class<?> productClass = Class.forName(productType);
                 T product = (T) productClass.getConstructor(int.class).newInstance(counterProduct);
                 storage.pushComponent(product);
                 ++counterProduct;
-            } catch (ClassNotFoundException | NoSuchMethodException | InterruptedException |
-                     InvocationTargetException | InstantiationException | IllegalAccessException e){
+            } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
+                     InstantiationException | IllegalAccessException e){
                 e.printStackTrace();
             }
         }
