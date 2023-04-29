23d29480467a5a438b7e31711269bd739892a266
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-04-29 14:34:47 +0700

    Update Car.java

--- JAVA_OOP/fabric/factory/product/Car.java
+++ JAVA_OOP/fabric/factory/product/Car.java
@@ -3,7 +3,7 @@ package factory.product;
 import factory.Product;
 
 public class Car extends Product {
-    public Car(int id) {
+    public Car(int id, Engine engine, Body body, Accessory accessory) {
         super(id);
     }
 }
