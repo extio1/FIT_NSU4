c8f99b59109b530a83db9fdca5b5d571f1282053
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-05-10 11:00:53 +0700

    Update CarStorageController.java

--- JAVA_OOP/carFactory/src/factory/carStorageController/CarStorageController.java
+++ JAVA_OOP/carFactory/src/factory/carStorageController/CarStorageController.java
@@ -1,33 +1,43 @@
 package factory.carStorageController;
 
 import factory.Storage;
+import factory.product.Accessory;
+import factory.product.Body;
 import factory.product.Car;
 import factory.job.CreateCar;
+import factory.product.Engine;
 import threadpool.PoolExecutor;
 
 public class CarStorageController extends Thread {
-    private final Storage<Car> storage;
+    private final Storage<Car> carStorage;
+    private final Storage<Body> bodyStorage;
+    private final Storage<Engine> engineStorage;
+    private final Storage<Accessory> accessoryStorage;
     private final PoolExecutor workers;
 
-    public CarStorageController(Storage<Car> storage, PoolExecutor workers){
-        this.storage = storage;
+    public CarStorageController(Storage<Car> carStorage, Storage<Engine> engineStorage, Storage<Body> bodyStorage,
+                                Storage<Accessory> accessoryStorage, PoolExecutor workers){
+        this.carStorage = carStorage;
+        this.bodyStorage = bodyStorage;
+        this.engineStorage = engineStorage;
+        this.accessoryStorage = accessoryStorage;
         this.workers = workers;
         this.setName("Car storage controller thread");
-        for(int i = 0; i < storage.getCapacity(); ++i){
-            workers.execute(new CreateCar());
+        for(int i = 0; i < carStorage.getCapacity(); ++i){
+            workers.execute(new CreateCar(engineStorage, bodyStorage, accessoryStorage, carStorage));
         }
     }
 
     @Override
     public void run(){
-        synchronized (storage) {
+        synchronized (carStorage) {
             while(!Thread.interrupted()){
                 try {
-                    storage.wait();
+                    carStorage.wait();
                 } catch (InterruptedException e) {
                     break;
                 }
-                workers.execute(new CreateCar());
+                workers.execute(new CreateCar(engineStorage, bodyStorage, accessoryStorage, carStorage));
             }
         }
     }
