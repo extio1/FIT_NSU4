0429307b2efa6b4a7c4fb09d42d9aeb60423d6c3
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-05-22 20:38:16 +0700

    Update server.c

--- OS/7lab/socket-tcp-echo-server-thread/server.c
+++ OS/7lab/socket-tcp-echo-server-thread/server.c
@@ -8,10 +8,13 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <poll.h>
+#include <arpa/inet.h>
 
 #define PORT 3500
 #define MAX_MESS_LEN 1000
 #define MAX_CLIENTS 10
+#define TIMEOUT 5000
 
 int srvsock;
 int clt_counter;
@@ -23,25 +26,6 @@ void* terminator(void* in){
 
 	close(srvsock);
 	exit(0);
-}
-
-void* handler(void* sockfd_ptr){
-	int sockfd = * (int*)sockfd_ptr;
-
-	char buffer[MAX_MESS_LEN];
-
-	while(1){
-		size_t len = 0;
-		if((len=read(sockfd, buffer, MAX_MESS_LEN)) < 0){
-			printf("read error: %s\n", strerror(errno));
-			continue;
-		}
-
-		if(write(sockfd, buffer, len) == -1){
-			printf("write error: %s\n", strerror(errno));
-			continue;
-		}
-	}
 }
 
 int main(){
@@ -75,16 +59,58 @@ int main(){
 
 	printf("Server starts at %d port, ready to serve %d\n", ntohs(srv.sin_port), ntohl(srv.sin_addr.s_addr));
 
+	struct pollfd clients[MAX_CLIENTS];
+	clients[0].fd = srvsock;
+	clients[0].events = POLLIN;
+
+	for(int i = 1; i < MAX_CLIENTS; ++i)
+		clients[i].fd = -1;
+
+	char buffer[MAX_MESS_LEN];
 	while(1){
-		int cltsock;
-		if( (cltsock = accept(srvsock, (struct sockaddr*)&srv, &len)) == -1){
-			perror("accept() error");
+		int nready = poll(clients, MAX_CLIENTS, -1);
+
+		if(nready == 0){
+			printf("timeout\n");
 			continue;
-		} else {
-			pthread_t tid;
-			if( pthread_create(&tid, NULL, handler, &cltsock) == -1){
-				perror("thread create error");
-				continue;
+		} else if(nready == -1){
+			perror("poll() error");
+		}
+
+		if(clients[0].revents & POLLIN){
+			if(clients[0].revents & POLLERR){
+				printf("Error on listenning socket\n");
+			}
+			int cltfd;
+			if( (cltfd = accept(srvsock, (struct sockaddr*)&clt, &len)) == -1){
+				printf("Error accepting new client\n");
+			}
+
+			printf("New client accepted on socket %d\naddr:%s\nport%d\n",
+				cltfd, inet_ntoa(clt.sin_addr), ntohs(clt.sin_port));
+			for(int i = 1; i < MAX_CLIENTS; ++i){
+				if(clients[i].fd == -1){
+					clients[i].fd = cltfd;
+					clients[i].events = POLLIN;
+					break;
+				}
+			}
+		}
+
+		for(int i = 1; i < MAX_CLIENTS; ++i){
+			if(clients[i].fd != -1){
+				if(clients[i].revents & POLLHUP){
+					close(clients[i].fd);
+					clients[i].fd = -1;
+					printf("Client on %d desc hung up\n", clients[i].fd);
+				} else if(clients[i].revents & POLLERR){
+					printf("Error on %d desc client\n", clients[i].fd);
+				} else if(clients[i].revents & POLLIN){
+					int nread = read(clients[i].fd, buffer, MAX_MESS_LEN);
+					if(nread != write(clients[i].fd, buffer, nread)){
+						fprintf(stderr, "Writing to client on %d socker error:%s\n", clients[i].fd, strerror(errno));
+					}
+				}
 			}
 		}
 	}
