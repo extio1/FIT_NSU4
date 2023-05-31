a2d4336d52e7eda38c0655df0f220378af4690d8
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-05-31 15:04:09 +0700

    Update MessageUserReq.java

--- JAVA_OOP/networkChat/src/protocol/clientObject/request/MessageUserReq.java
+++ JAVA_OOP/networkChat/src/protocol/clientObject/request/MessageUserReq.java
@@ -4,20 +4,29 @@ import client.ClientSessionData;
 import protocol.ObjectUser;
 import protocol.Request;
 import protocol.Response;
+import protocol.serverObject.response.SuccessAnswerServerResp;
 import protocol.visitor.VisitorClient;
 
+import java.io.Serializable;
 
-public class MessageUserReq extends ObjectUser implements Request {
+public class RegisterUserReq extends ObjectUser implements Request {
     private final String data;
-    public MessageUserReq(String message){
+    public RegisterUserReq(String name){
         super();
-        data = message;
+        data = name;
     }
-    public String getMessage() {
+    public String getName() {
         return data;
     }
     public void accept(VisitorClient v) {
-        v.visitMessageRequest(this);
+        v.visitRegisterUserRequest(this);
     }
-    public void handleServerResponse(Response r, ClientSessionData data) {}
+    public void handleServerResponse(Response r, ClientSessionData data) {
+        if(r instanceof SuccessAnswerServerResp s){
+            if(s.getChatHistory() != null)
+                data.setChatHistory(s.getChatHistory());
+            if(s.getUserName() != null)
+                data.setClientName(s.getUserName());
+        }
+    }
 }
