f810257712bdc0332beecd248e0e5d485ef99dc9
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-05-31 14:50:22 +0700

    Update ReceiverClientJava.java

--- JAVA_OOP/networkChat/src/client/reciever/java/ReceiverClientJava.java
+++ JAVA_OOP/networkChat/src/client/reciever/java/ReceiverClientJava.java
@@ -4,10 +4,12 @@ import client.reciever.ReceiverClient;
 import protocol.Event;
 import protocol.ObjectServer;
 import protocol.Response;
+import protocol.serverObject.response.SuccessAnswerServerResp;
 import protocol.visitor.VisitorServer;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 
 public class ReceiverClientJava extends ReceiverClient {
@@ -24,6 +26,9 @@ public class ReceiverClientJava extends ReceiverClient {
                 srvMess.accept(visitorServer);
             } catch (IOException | ClassNotFoundException e) {
                 System.out.println("Receiver clt "+e.getMessage());
+                if(e instanceof SocketException){
+                    break;
+                }
             }
         }
     }
